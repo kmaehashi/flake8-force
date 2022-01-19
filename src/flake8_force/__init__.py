d11d0199c32514c6982d147524c94544e4f9b8bc
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2022-01-19 17:01:22 +0000

    do not interfere when the option is disabled

--- src/flake8_force/__init__.py
+++ src/flake8_force/__init__.py
@@ -6,16 +6,15 @@ import flake8.checker
 class ForceFileChecker(flake8.checker.FileChecker):
     """FileChecker that ignores AST build failure."""
 
-    def run_ast_checks(self, *args, **kwargs):
-        if self.options.force_check:
-            exc_types = (SyntaxError, tokenize.TokenError)
-        else:
-            exc_types = ()
+    def run_ast_checks(self, *args, **kwargs) -> None:
+        if not self.options.force_check:
+            # Do nothing if the option is not specified.
+            return super().run_ast_checks(*args, **kwargs)
 
         try:
             super().run_ast_checks(*args, **kwargs)
-        except exc_types as e:
-            if hasattr(self, '_extract_syntax_information'):
+        except (SyntaxError, tokenize.TokenError) as e:
+            if hasattr(self, "_extract_syntax_information"):
                 row, column = self._extract_syntax_information(e)
             else:
                 (row, column) = (1, 1)
